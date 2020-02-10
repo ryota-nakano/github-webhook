6213953a0cb13ad0ee660f1b81bd381a4a3b13fb
中野 亮太 <rnakanor@gmail.com>
2020-02-10 21:47:38 +0900

    ファイル追加

---
+++ github-webhook.php
@@ -0,0 +1,45 @@
+<?php
+
+$USER = '';# Github ユーザー名
+$PASS = '';# Github パスワード
+$SECRET = '';#Github Webhook Secret
+
+/*===================================================
+	
+	# GithubのWebhookを使った自動デプロイ
+		* webhookのURLをこのファイルにする
+		* git clone / git pull を両方
+		* webhook.logファイル生成
+	
+===================================================*/
+
+$sig = $_SERVER['HTTP_X_HUB_SIGNATURE'];#sha1=xxx...
+$hmac = 'sha1='.hash_hmac('sha1',file_get_contents('php://input'),$SECRET);#sha1=xxx...を生成
+$date = date("[Y-m-d H:i:s]");# 日付
+$adder = $_SERVER['REMOTE_ADDR'];# 訪問者のIPアドレス
+
+# Githubからのアクセス
+if(hash_equals($hmac,$sig)){
+	$payload = json_decode($_POST['payload'],true);
+	$dir = $payload['repository']['name'];
+	$clone_url = str_replace('https://', "https://{$USER}:{$PASS}@", $payload['repository']['clone_url']);
+	$ref = $payload['ref'];# ブランチ判定
+	# 既にクローンされている場合
+	if(is_dir($dir)){
+		$log = "{$date} {$adder}: {$ref} がプッシュされました。";
+		if($ref=='refs/heads/master'){
+			exec("cd {$dir};git pull origin master");
+			$log.= "{$dir}にmasterをプルしました。";
+		}
+	}
+	# クローンされていない場合
+	else{
+		exec("git clone {$clone_url}");
+		$log = "{$date} {$adder}: リポジトリをクローンしました。";
+	}
+}
+# Github以外からのアクセス
+else{
+	$log = "{$date} {$adder}: 不正アクセス。";
+}
+file_put_contents('github-webhook.log', "{$log}\n", FILE_APPEND|LOCK_EX);# ログファイル生成
